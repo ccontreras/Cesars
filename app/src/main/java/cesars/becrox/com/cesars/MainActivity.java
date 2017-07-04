c825efc9d8cd98b4e73a6859597bb6a51853abaf
César Contreras <ccontreras@orange.com.do>
2017-07-04 12:02:22 -0400

    Added about-me sidebar.

--- app/src/main/java/cesars/becrox/com/cesars/MainActivity.java
+++ app/src/main/java/cesars/becrox/com/cesars/MainActivity.java
@@ -4,43 +4,107 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cesars.becrox.com.cesars.util.Utils;
-import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
+import me.kaelaela.verticalviewpager.VerticalViewPager;
+import me.kaelaela.verticalviewpager.transforms.DefaultTransformer;
+import me.relex.circleindicator.CircleIndicator;
+
+/**
+ * @author cconTreras
+ */
 
 public class MainActivity extends AppCompatActivity {
 
-  @BindView(R.id.my_photos_pager) ViewPager mPhotosViewPager;
+  /**
+   * This is the {@link ViewPager} that will render
+   * all the images about me.
+   */
+  @BindView(R.id.photoPager) /*package*/ VerticalViewPager mPhotoViewPager;
 
-  private MyPhotosAdapter mPhotosAdapter;
+  /**
+   * This is the {@link CircleIndicator} for the photos that is displayed
+   * at the right.
+   */
+  @BindView(R.id.indicator) /*package*/ CircleIndicator mPhotoIndicator;
+
+  /**
+   * This is a reference to the {@link ViewGroup} that contains
+   * information about me.
+   */
+  @BindView(R.id.aboutContainer) /*package*/ ViewGroup mAboutContainer;
+
+  /**
+   * An instance of {@link PhotoAdapter} to using with
+   * the {@link #mPhotoViewPager}.
+   */
+  /*package*/ PhotoAdapter mPhotosAdapter;
+
+  static {
+    AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Utils.hideStatusBar(this);
 
     ButterKnife.bind(this);
 
-    int[] myPhotos = { R.drawable.me1, R.drawable.me2, R.drawable.me3 };
-    mPhotosAdapter = new MyPhotosAdapter(this, myPhotos);
-    mPhotosViewPager.setAdapter(mPhotosAdapter);
+    // Load photos.
+    int[] photos = { R.drawable.me1, R.drawable.me2, R.drawable.me3 };
+    mPhotosAdapter = new PhotoAdapter(this, photos);
+    mPhotoViewPager.setAdapter(mPhotosAdapter);
+    mPhotoViewPager.setPageTransformer(false, new DefaultTransformer());
+    mPhotoIndicator.setViewPager(mPhotoViewPager);
 
+    Utils.applyDefaultTypeface(mAboutContainer, getAssets());
   }
 
-  private class MyPhotosAdapter extends PagerAdapter {
+  @Override public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) Utils.hideStatusBar(this);
+  }
 
+  /**
+   * This adapter creates an {@link ImageView} for every
+   * drawable in the array of {@link #mPhotos}. I've seen applications that
+   * implement {@link FragmentPagerAdapter} just for showing images and that can
+   * be painful for that task, so I just simply create an {@link ImageView} to
+   * to show my photos with no need to handle {@link Fragment}s lifecycles.
+   */
+  private class PhotoAdapter extends PagerAdapter {
+
+    private static final int RESIZE_WIDTH = 600; // in pixels
+    private static final int RESIZE_HEIGHT = 400; // in pixels
+
+    /**
+     * The current context.
+     */
     private @NonNull Context mContext;
+
+    /**
+     * An array of drawable identifiers.
+     */
     private @DrawableRes int[] mPhotos;
 
-    public MyPhotosAdapter(@NonNull Context context, @DrawableRes int[] photos) {
+    /**
+     * Creates a new instance of this adapter.
+     *
+     * @param context the current context.
+     * @param photos an array of drawables identifiers.
+     */
+    /*package*/ PhotoAdapter(@NonNull Context context, @DrawableRes int[] photos) {
       this.mContext = context;
       this.mPhotos = photos;
     }
@@ -59,7 +123,12 @@ public class MainActivity extends AppCompatActivity {
           ViewGroup.LayoutParams.MATCH_PARENT));
       image.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
-      Picasso.with(mContext).load(mPhotos[position]).resize(600, 400).centerCrop().into(image);
+      Picasso.with(mContext)
+          .load(mPhotos[position])
+          .noFade()
+          .resize(RESIZE_WIDTH, RESIZE_HEIGHT)
+          .centerCrop()
+          .into(image);
 
       container.addView(image);
       return image;
--- app/src/main/java/cesars/becrox/com/cesars/util/Utils.java
+++ app/src/main/java/cesars/becrox/com/cesars/util/Utils.java
@@ -2,7 +2,11 @@ package cesars.becrox.com.cesars.util;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * @author cconTreras
@@ -10,6 +14,24 @@ import android.view.View;
 
 public final class Utils {
 
+  /**
+   * This is the path for the base typeface used across the app.
+   */
+  private static final String DEFAULT_TYPEFACE_PATH = "fonts/SuezOne-Regular.ttf";
+
+  /**
+   * This references the default {@link Typeface} instance to
+   * set for all text widgets.
+   */
+  private static Typeface sDefaultTypeface;
+
+  /**
+   * Hides the status bar.
+   *
+   * https://developer.android.com/training/system-ui/status.html
+   *
+   * @param activity the current activity.
+   */
   public static void hideStatusBar(Activity activity) {
     View decorView = activity.getWindow().getDecorView();
     // Hide the status bar.
@@ -18,5 +40,41 @@ public final class Utils {
     // status bar is hidden, so hide that too if necessary.
     ActionBar actionBar = activity.getActionBar();
     if (actionBar != null) actionBar.hide();
+  }
+
+  /**
+   * This method searches for {@link TextView}s widgets in a {@link ViewGroup}
+   * to set the default {@link Typeface} to it when found.
+   *
+   * @param viewGroup the {@link ViewGroup} to search in.
+   * @param assetManager the {@link AssetManager} to load the {@link Typeface}.
+   */
+  public static void applyDefaultTypeface(ViewGroup viewGroup, AssetManager assetManager) {
+    View view;
+    for (int i = 0; i < viewGroup.getChildCount(); i++) {
+      view = viewGroup.getChildAt(i);
+      if (view instanceof ViewGroup) {
+        applyDefaultTypeface(((ViewGroup) view), assetManager);
+      } else if (view instanceof TextView) {
+        ((TextView) view).setTypeface(getDefaultTypeface(assetManager));
+      }
+    }
+  }
+
+  /**
+   * This method initializes, loads and returns the {@link Typeface}.
+   *
+   * @param assetManager the {@link AssetManager} to load the font.
+   * @return a {@link Typeface} instance.
+   */
+  public static Typeface getDefaultTypeface(AssetManager assetManager) {
+    if (sDefaultTypeface == null) {
+      synchronized (Utils.class) {
+        if (sDefaultTypeface == null) {
+          sDefaultTypeface = Typeface.createFromAsset(assetManager, DEFAULT_TYPEFACE_PATH);
+        }
+      }
+    }
+    return sDefaultTypeface;
   }
 }
